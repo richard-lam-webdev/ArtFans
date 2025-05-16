6d68de42afacc218e706a0b27a92e081d35006bb
richard-lam-dev <richard.lam1998@gmail.com>
2025-05-16 10:46:47 +0200

    wip

--- frontend/integration_test/app_test.dart
+++ frontend/integration_test/app_test.dart
@@ -1,14 +1,15 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:frontend/main.dart' as app;  // <-- remplace "your_app" par le nom de ton package
+import 'package:frontend/main.dart'
+    as app; // <-- remplace "your_app" par le nom de ton package
 
-void main() {
+void main() async {
   // 1️⃣ Initialisation du binding
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   // 2️⃣ Appelle ton application
   app.main();
-  await Future<void>.delayed(const Duration(seconds: 1)); 
+  await Future<void>.delayed(const Duration(seconds: 1));
   // (optionnel) attendre que runApp() ait bien construit le widget
 
   // 3️⃣ Écrire tes tests
