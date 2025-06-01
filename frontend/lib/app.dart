a216514e84792d7d45d8a92c8e3eef0263020206
richard-lam-webdev <richard.lam1998@gmail.com>
2025-06-01 21:32:43 +0200

    fix const error

--- frontend/lib/app.dart
+++ frontend/lib/app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'pages/sign_in_page.dart';
-import 'pages/home_page.dart'; // À créer ensuite pour la page d’accueil
+import 'pages/home_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
