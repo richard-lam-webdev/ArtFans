9b12b25437306a96b58890bd9e696428c9bf334a
Dams25 <damien2504@outlook.fr>
2025-07-01 23:46:34 +0200

    feat: add Prometheus and Grafana services for monitoring, update dependencies, and configure metrics endpoint

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/richard-lam-webdev/ArtFans/backend/internal/config"
 	"github.com/richard-lam-webdev/ArtFans/backend/internal/database"
 	"github.com/richard-lam-webdev/ArtFans/backend/internal/handlers"
@@ -64,6 +65,8 @@ func main() {
 
 	/* ---------- 8) Contenus publics ---------- */
 	r.GET("/api/contents", contentHandler.GetAllContents)
+
+	r.GET("/metrics", gin.WrapH(promhttp.Handler()))
 
 	/* ---------- 9) Routes protégées JWT ---------- */
 	protected := r.Group("/api", middleware.JWTAuth())
