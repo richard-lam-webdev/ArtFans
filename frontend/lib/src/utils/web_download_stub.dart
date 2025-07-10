fe9cdb1c45fe4ebd1d3debad8c2263a99b8bf014
Dams25 <damien2504@outlook.fr>
2025-07-10 23:04:44 +0200

    Correction bug

--- frontend/lib/src/utils/web_download_stub.dart
+++ frontend/lib/src/utils/web_download_stub.dart
@@ -1,4 +1,4 @@
-import 'dart:typed_data';
+import 'package:flutter/foundation.dart';
 
 void downloadFileWeb(Uint8List bytes, String filename) {
   throw UnsupportedError('Web downloads are not supported on this platform');
--- frontend/lib/src/utils/web_download_web.dart
+++ frontend/lib/src/utils/web_download_web.dart
@@ -1,5 +1,4 @@
-// lib/src/utils/web_download_web.dart
-import 'dart:typed_data';
+import 'package:flutter/foundation.dart';
 // ignore: deprecated_member_use, avoid_web_libraries_in_flutter
 import 'dart:html' as html;
 
