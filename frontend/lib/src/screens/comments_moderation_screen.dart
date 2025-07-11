d805b8b1ca2e8d250f61a826aa39c272b9f406d9
richard-lam-dev <richard.lam1998@gmail.com>
2025-07-11 11:12:50 +0200

    fix

--- frontend/lib/src/screens/comments_moderation_screen.dart
+++ frontend/lib/src/screens/comments_moderation_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:go_router/go_router.dart';
 
 import '../providers/comment_moderation_provider.dart';
 
