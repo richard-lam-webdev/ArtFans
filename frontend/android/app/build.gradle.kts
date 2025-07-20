d97c2ef05edb57e6f1d681494cff14f681a89ae3
richard-lam-dev <richard.lam1998@gmail.com>
2025-07-20 23:39:01 +0200

    Ajout de l'APK Android release v1.0.0

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -1,49 +1,61 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("dev.flutter.flutter-gradle-plugin")
+  id("com.android.application")
+  id("kotlin-android")
+  id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.example.frontend"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+  namespace = "fr.ddasilva.artfans"
+  compileSdk = flutter.compileSdkVersion
+  ndkVersion = "27.0.12077973"
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+  }
+  kotlinOptions {
+    jvmTarget = JavaVersion.VERSION_11.toString()
+  }
+
+  defaultConfig {
+    applicationId = "fr.ddasilva.artfans"
+    minSdk        = flutter.minSdkVersion
+    targetSdk     = flutter.targetSdkVersion
+    versionCode   = flutter.versionCode
+    versionName   = flutter.versionName
+  }
+
+  signingConfigs {
+    create("release") {
+      val propsFile = rootProject.file("../key.properties")
+      val props = Properties().apply {
+        load(FileInputStream(propsFile))
+      }
+      storeFile     = file(props["storeFile"] as String)
+      storePassword = props["storePassword"] as String
+      keyAlias      = props["keyAlias"] as String
+      keyPassword   = props["keyPassword"] as String
     }
+  }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+  buildTypes {
+    getByName("release") {
+      isMinifyEnabled = true
+      proguardFiles(
+        getDefaultProguardFile("proguard-android.txt"),
+        "proguard-rules.pro"
+      )
+      signingConfig = signingConfigs.getByName("release")
     }
-
-    defaultConfig {
-        applicationId = "com.example.frontend"
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+    getByName("debug") {
+      isMinifyEnabled = false
     }
-
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = true
-
-            proguardFiles(
-                getDefaultProguardFile("proguard-android.txt"),
-                "proguard-rules.pro"
-            )
-
-            signingConfig = signingConfigs.getByName("debug")
-        }
-        getByName("debug") {
-            isMinifyEnabled = false
-        }
-    }
+  }
 }
 
 flutter {
-    source = "../.."
+  source = "../.."
 }
